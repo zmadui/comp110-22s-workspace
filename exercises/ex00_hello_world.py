0c04970596be444a6e6da7f2c75731249688619c
Zoe <zmadui@live.unc.edu>
2022-01-17 19:26:19 -0500

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,3 @@
+"""My first program for COMP110"""
+print ("Hello, Welcome to Zoe's world.")
+__author__ = 730329470
---
+++ lessons/00_stored_program.py
@@ -0,0 +1,6 @@
+"""An example, simple program."""
+
+print("Hello, world")
+quiz: int = 100
+print(quiz)
+print("Goodbye.")
